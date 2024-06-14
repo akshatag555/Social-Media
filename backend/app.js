d8abcfa97a90e53c859f876b8a8c90ca31ec62be
Akshat Agarwal <akshatag55@gmail.com>
2024-06-15 01:54:21 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,20 @@ const express=require('express');
 const app=express();
 const cookieParser=require("cookie-parser")
 var cors = require('cors')
-if(process.env.NODE_ENV!=="producion") require("dotenv").config({path:"backend/config/config.env"})
+if(process.env.NODE_ENV!=="production") require("dotenv").config({path:"backend/config/config.env"})
 //using middleware
+const corsOptions = {
+    origin: 'https://social-media-d5i3.vercel.app/', // replace with your frontend domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // allows cookies to be sent
+    optionsSuccessStatus: 204
+  };
+  app.use(cors(corsOptions));
+
 app.use(express.json({limit:'50mb'}))
 app.use(express.json());
 app.use(express.urlencoded({limit:'50mb',extended:true}));
 app.use(cookieParser());
-app.use(cors())
 //importing route
  const post=require("./routes/posts");
 const user=require("./routes/user");
