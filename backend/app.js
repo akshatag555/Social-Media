304c0cf4222399402e3bea355219dd5c854e6550
Akshat Agarwal <akshatag55@gmail.com>
2024-06-14 21:38:43 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 const express=require('express');
 const app=express();
 const cookieParser=require("cookie-parser")
+var cors = require('cors')
 if(process.env.NODE_ENV!=="producion") require("dotenv").config({path:"backend/config/config.env"})
 //using middleware
 app.use(express.json({limit:'50mb'}))
 app.use(express.json());
 app.use(express.urlencoded({limit:'50mb',extended:true}));
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors({
+    origin:["*"]
+}));
 //importing route
  const post=require("./routes/posts");
 const user=require("./routes/user");
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const app=require("./app");
 const { connectDatabase } = require("./config/database");
 const cloudinary=require("cloudinary");
 connectDatabase();
+
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
