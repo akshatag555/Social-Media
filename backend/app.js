7f3c80971f490d3e2a2c036372e7e83a47dbc8ec
Akshat Agarwal <akshatag55@gmail.com>
2024-06-14 21:42:02 +0530

    cors update

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,7 @@ app.use(express.json({limit:'50mb'}))
 app.use(express.json());
 app.use(express.urlencoded({limit:'50mb',extended:true}));
 app.use(cookieParser());
-app.use(cors({
-    origin:"*"
-}));
+app.use(cors())
 //importing route
  const post=require("./routes/posts");
 const user=require("./routes/user");
